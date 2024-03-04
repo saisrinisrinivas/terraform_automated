c51e23e23d1f1f7515c7d6ea19ef1cf1bebfe68f
saisrinisrinivas <vsaisrinivas182000@gmail.com>
2024-03-04 08:18:47 +0530

    -a

---
+++ c2-ec2instance.tf
@@ -0,0 +1,17 @@
+# # Terraform Settings Block
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0" # Optional but recommended in production
+#     }
+#   }
+# }
+
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+  
+}
