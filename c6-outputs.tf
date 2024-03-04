1d724e44459c1c9a56545edd4bdf7204368c9bc1
saisrinisrinivas <vsaisrinivas182000@gmail.com>
2024-03-04 16:11:48 +0530

    -a

--- c6-outputs.tf
+++ c6-outputs.tf
@@ -1,13 +1,13 @@
 # Terraform Output Values
 
 # EC2 Instance Public IP
-output "instance_publicip" {
+output "instance_publicdp" {
   description = "EC2 Instance Public IP"
   value = aws_instance.ec2demo.public_ip
 }
 
 # EC2 Instance Public DNS
-output "instance_publicdns" {
+output "instance_publickns" {
   description = "EC2 Instance Public DNS"
   value = aws_instance.ec2demo.public_dns
 }
