544459376da817619fe622371631f07255977e20
saisrinisrinivas <vsaisrinivas182000@gmail.com>
2024-03-04 16:10:22 +0530

    -a

--- c2-ec2instance.tf
+++ c2-ec2instance.tf
@@ -13,7 +13,7 @@
 resource "aws_instance" "ec2demo" {
   ami           = data.aws_ami.amzlinux2.id # Amazon Linux in us-east-1, update as per your region
   instance_type = var.instance_type
-  vpc_security_group_ids = [ "aws_security_group.vpc-ssh.id", "aws_security_group.vpc-web.id"]
+  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   user_data = file("${path.module}/app1-install.sh")
   tags = {
     "Name" = "EC2 Demo"
