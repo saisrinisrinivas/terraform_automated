5b57cf47440b811d39d1209122ca882e31723250
saisrinisrinivas <vsaisrinivas182000@gmail.com>
2024-03-10 11:29:07 +0530

    -a

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 module "ec2_instance" {
-    source = "./modules/ec2-instances/c1-versions.tf"
+    source = "./modules/ec2-instances/c2-versions.tf"
     instance_type = "t2.large"
 
 }
