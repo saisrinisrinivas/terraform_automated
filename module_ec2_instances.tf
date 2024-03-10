d939abe9cfcab68d32f1dfe94919d8f296b4b11e
saisrinisrinivas <vsaisrinivas182000@gmail.com>
2024-03-10 13:08:48 +0530

    -a

--- module_ec2_instances.tf
+++ module_ec2_instances.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket = "srinis001-terra-bucket"
-    key    = "terraformstate/terraform.tfstate"
-    region = "us-east-1"
+  # backend "s3" {
+  #   bucket = "srinis001-terra-bucket"
+  #   key    = "terraformstate/terraform.tfstate"
+  #   region = "us-east-1"
     
-  }
+  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- module_s3_bucket.tf
+++ module_s3_bucket.tf
@@ -1,5 +1,5 @@
 
 module "s3_buckets" {
     source = "./modules/s3_bucket"
-    bucket_name = "srinis001-terra-bucket"
+    bucket_name = "heyhi123"
 }
