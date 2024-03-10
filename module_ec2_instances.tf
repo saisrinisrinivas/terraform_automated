40d11fb46741b5a11e6b5248a54a53ebbfa78897
saisrinisrinivas <vsaisrinivas182000@gmail.com>
2024-03-10 13:17:23 +0530

    -a

--- module_ec2_instances.tf
+++ module_ec2_instances.tf
@@ -1,6 +1,6 @@
 terraform {
   # backend "s3" {
-  #   bucket = "srinis001-terra-bucket"
+  #   bucket = "heyhi123"
   #   key    = "terraformstate/terraform.tfstate"
   #   region = "us-east-1"
     
