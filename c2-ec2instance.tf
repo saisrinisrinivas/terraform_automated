147b053a806006bc78eb114ad419f43f7eb4f1d9
saisrinisrinivas <vsaisrinivas182000@gmail.com>
2024-03-24 13:48:04 +0530

    -a

--- c2-ec2instance.tf
+++ c2-ec2instance.tf
@@ -1,12 +1,22 @@
+data "aws_availability_zones" "my_azones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+
+
 # Resource Block
 resource "aws_instance" "ec2demo" {
   ami           = data.aws_ami.amzlinux2.id # Amazon Linux in us-east-1, update as per your region
   instance_type = var.instance_type_map["qa"]
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   user_data = file("${path.module}/app1-install.sh")
-  count = 2
+  # count = 2
+  for_each = data.aws_availability_zones.my_azones.names
   tags = {
-    "Name" = "Count-Demo-${count.index}"
+    "Name" = "For-Each-Demo-${each.key}"
    }
 
   
--- c6-outputs.tf
+++ c6-outputs.tf
@@ -12,19 +12,31 @@
 #   value = aws_instance.ec2demo.public_dns
 # }
 
-output "for_output_list" {
-  description = "For loop with lists"
-  value = [for ins in aws_instance.ec2demo: ins.public_ip]
+# output "for_output_list" {
+#   description = "For loop with lists"
+#   value = [for ins in aws_instance.ec2demo: ins.public_ip]
+  
+# }
+
+# output "for_output_map" {
+#   description = "For loop with map"
+#   value = {for ins in aws_instance.ec2demo: ins.id => ins.public_ip}
+  
+# }
+
+# output "latest_splat_operator"{
+#   description = "Generalized latest splat operator"
+#   value = aws_instance.ec2demo[*].public_dns
+# }
+
+output "for_each_dns" {
+  description = "Using for_each to output the public_dns"
+  value = toset([for instance in aws_instance.ec2_demo: instance.public_dns])
   
 }
 
-output "for_output_map" {
-  description = "For loop with map"
-  value = {for ins in aws_instance.ec2demo: ins.id => ins.public_ip}
+output "for_each_ip" {
+  description = "Using for_each to output the public_dns"
+  value = toset([for instance in aws_instance.ec2_demo: instance.public_ip])
   
-}
-
-output "latest_splat_operator"{
-  description = "Generalized latest splat operator"
-  value = aws_instance.ec2demo[*].public_dns
 }
